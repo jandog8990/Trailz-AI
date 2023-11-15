4f181f33433feccb343e132463cda57f59ae0d27
Jando Gonzales <jandog8990@gmail.com>
2023-11-14 20:38:18 -0700

    Added new class for creating route description map

--- ParseMTBProject/ParseMTB.py
+++ ParseMTBProject/ParseMTB.py
@@ -10,6 +10,9 @@ URL = "https://www.mtbproject.com/trail/4939737/hangover-loop"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
+# TODO: Think of how to separate this into classes so when we bring 
+# in multiple trails we can parse them accordingly
+
 def createTrailMap():
     # titlebar
     titlebar = soup.find(id="title-bar")
@@ -35,9 +38,8 @@ def trailMapContents(trailMap):
         val.show_contents()
         print("\n")
 
-def createTrailMetadata():
+def createTrailMetadata(trailTitle):
     # header
-    trailTitle = soup.find(id="trail-title")
     trailTitle = trailTitle.text.strip()
 
     # split the trail name so we can set the id
@@ -69,13 +71,7 @@ def createTrailMetadata():
         "num_ratings": numRatings
     }
 
-def createMainSectionHeaders():
-    # main content area
-    trailText = soup.find(id="trail-text")
-
-    #get the FEATURES
-    features = trailText.find_all("h3", class_="mr-2")
-
+def createMainSectionHeaders(trailText):
     # let's try and find the main text header sections
     h3Tags = trailText.find_all("h3")
     h3Count = 0
@@ -90,17 +86,88 @@ def createMainSectionHeaders():
 
     return mainSectionHeaders
 
+# create a function that takes in main text and parses
+def parseMainText(trailText):
+    # Main section text starts at index = 2
+    mainText = trailText.find_all("div", class_="mb-1")
+    start = 2
+    end = len(mainText)
+    count = 0
+    mainBody = []
+    for idx in range(start, end-1):
+        elem = mainText[idx]
+        strippedText = elem.text.strip()
+        newText = re.sub(' +', ' ', strippedText)
+        newText = re.sub('\n', '\n', newText)
+        mainBody.append(newText)
+        count+=1
+    
+    return mainBody
+
+# create the trail route descriptions from body text and headers
+def createTrailRouteDescriptions(trailId, mainSectionHeaders, bodyText):
+    # Let's create the dictionary of section headers to main text
+    mainTextMap = {}
+    start = 0
+    end = len(mainSectionHeaders)-1
+    for i in range(start, end):
+        header = mainSectionHeaders[i]
+        text = bodyText[i]
+        mainTextMap[header] = text
+
+    mtb_trail_route_descriptions = []
+    print("Main map size = " + str(len(mainTextMap)))
+    for key in mainTextMap:
+        description = mainTextMap[key]
+        print("Key = {0}".format(key))
+        print("Value = {0}".format(mainTextMap[key]))
+        print("\n")
+        
+        descObj = {
+            "_id": key.lower(),
+            "key": key,
+            "text": description,
+            "mtb_trail_route_id": trailId 
+        }
+        mtb_trail_route_descriptions.append(descObj)  
+    return mtb_trail_route_descriptions
+
 # create the trail map and print
 trailMap = createTrailMap()
 trailMapContents(trailMap)
 
 # create the trail metadata
-trailMetadata = createTrailMetadata() 
+trailTitle = soup.find(id="trail-title")
+trailMetadata = createTrailMetadata(trailTitle) 
 trailMetadata["trail_area"] = trailMap
 print("Trail metadata:")
 print(trailMetadata)
 print("\n")
 
+# main text for trail descriptions 
+trailText = soup.find(id="trail-text")
+
 # create main section headers for trail text
-mainSectionHeaders = createMainSectionHeaders()
+mainSectionHeaders = createMainSectionHeaders(trailText)
 print(mainSectionHeaders)
+print("\n")
+
+# get the mtb body text from trail text element
+bodyText = parseMainText(trailText)
+print(bodyText)
+print("\n")
+
+# create the mtb trail route descriptions
+trailTitle = trailTitle.text.strip()
+print("Trail title = " + trailTitle)
+
+# split the trail name so we can set the id
+trailTokens = trailTitle.split()
+trailId = trailTokens[0].lower()
+print("Trail id = " + trailId)
+
+mtbTrailRouteDescriptions = createTrailRouteDescriptions(trailId, mainSectionHeaders, bodyText)
+print("Number of descriptions = " + str(len(mtbTrailRouteDescriptions)))
+print(mtbTrailRouteDescriptions)
+
+# next connect to the database
