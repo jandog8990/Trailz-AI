76b5079d6fe5b923b7ca995a9415fe4874e2626b
Jando Gonzales <jandog8990@gmail.com>
2024-01-05 18:31:43 -0700

    Updated code to now create dataset objects to be used to upsert to the pinecone vector db

--- ParseMTBProject/PineConeSearch.py
+++ ParseMTBProject/PineConeSearch.py
@@ -17,9 +17,17 @@ with open('mtb_descs.pkl', 'rb') as f:
 print(f"Routes len = {len(mtb_routes)}")
 print(f"Descs len = {len(mtb_descs)}")
 
+# append to the area lists if the elements exist
+def append_area_lists(areaObj, areaNames, areaRefs): 
+    if 'areaName' in areaObj:
+        areaNames.append(areaObj["areaName"]) 
+    if 'areaRef' in areaObj:
+        areaRefs.append(areaObj["areaRef"]) 
+    return (areaNames, areaRefs)
+
 #TODO: We need to create a mapping of metadata to text
 # 1. Create a map of ids to multiple description objects
-combinedRoutes = [] 
+mainMTBRoutes = [] 
 count = 0
 for route in mtb_routes:
     # create the new route object from route and desc 
@@ -38,11 +46,9 @@ for route in mtb_routes:
    
     # parse out the state from trail area 
     trailArea = route['trail_area'] 
-    st = json.loads(trailArea['state'][0])
-    stateName = st["areaName"]
-    stateRef = st["areaRef"]
-    areaNames.append(stateName)
-    areaRefs.append(stateRef)
+    areaObj = json.loads(trailArea['state'][0])
+    areaNames.append(areaObj["areaName"])
+    areaRefs.append(areaObj["areaRef"])
 
     #TODO: Need to have this organized better
     # 1. The first element is always the State
@@ -55,30 +61,18 @@ for route in mtb_routes:
     if 'sub_area' in trailArea and (len(trailArea['sub_area']) != 0):
         
         # loop through the sub areas and create array 
-        print(f"Len sub area = {len(trailArea['sub_area'])}") 
         areaArr = trailArea['sub_area']
         for area in areaArr:
-            st = json.loads(area) 
-            areaNames.append(st["areaName"] if 'areaName' in st else "") 
-            areaRefs.append(st["areaRef"] if 'areaRef' in st else "") 
+            areaObj = json.loads(area) 
+            (areaNames, areaRefs) = append_area_lists(areaObj, areaNames, areaRefs) 
 
     # parse out trail system from area
-    st = json.loads(trailArea['trail_system'][0]) if 'trail_system' in trailArea and (len(trailArea['trail_system']) != 0) else {} 
-    cityTownName = st["areaName"] if 'areaName' in st else "" 
-    cityRef = st["areaRef"]
-    areaNames.append(cityTownName)
-    areaRefs.append(cityRef)
+    areaObj = json.loads(trailArea['trail_system'][0]) if 'trail_system' in trailArea and (len(trailArea['trail_system']) != 0) else {} 
+    (areaNames, areaRefs) = append_area_lists(areaObj, areaNames, areaRefs) 
   
     # metadata queries can be used by "current" location or
     # other area prompts such as the ones 
     # used by MTBProject & AllTrails
-    print(f"Trail url = {route['trail_url']}")
-    #print(f"State = {stateName}")
-    #print(f"Trail system = {cityTownName}")
-    print(f"Sub area names and refs:")
-    print(areaNames)
-    print(areaRefs)
-    print("\n")
 
     # TODO: The location needs to be more consistent
     # set the state, city and sub areas for the metadata
@@ -87,12 +81,9 @@ for route in mtb_routes:
 
     # need to put the trail area in a sentence description to be 
     # searched by the user query
-    trailRouteName = areaNames.pop(-1)  
+    trailRouteName = areaNames[-1] 
     trailRouteLocation = ", ".join(areaNames) 
     trailAreaSentence = "This mtb trail route lies in " + trailRouteName + " located in " + trailRouteLocation + "." 
-    print("New area names:")
-    print(trailAreaSentence)
-    print("\n")
 
     # create the main text from all descriptions
     descs = [desc for desc in mtb_descs 
@@ -101,31 +92,25 @@ for route in mtb_routes:
     textArr.insert(0, trailAreaSentence) 
     mainText = " ".join(textArr)
     count = count + 1
-   
+  
     # show the final main text
-    print("Main text:")
-    print(mainText)
-    print("\n")
-    
-    if count == 5:
-        break
+    newRouteObj['mainText'] = mainText 
 
+    # append to the main routes list
+    print(".", end="", flush=True) 
+    mainMTBRoutes.append(newRouteObj)
 
 # create datasets from lists
-"""
-routesDataset = Dataset.from_list(routeDataFrames)
-descsDataset = Dataset.from_list(descDataFrames)
-print("First routes dataset:")
-print(routesDataset[0])
+print(f"\nMain MTB Routes len = {len(mainMTBRoutes)}")
+print(mainMTBRoutes[0:2])
 print("\n")
 
-print("First descs dataset:")
-print(descsDataset[0])
+mtbRoutesDataset = Dataset.from_list(mainMTBRoutes)
+print(f"First routes dataset (len = {len(mtbRoutesDataset)}):")
+print(mtbRoutesDataset[0])
 print("\n")
-"""
 
 # connect to the pine cone api
-"""
 config = dotenv_values(".env")
 env_key = config["PINE_CONE_ENV_KEY"]
 api_key = config["PINE_CONE_API_KEY"]
@@ -133,7 +118,11 @@ print(f"env_key = {env_key}")
 print(f"api_key = {api_key}")
 print("\n")
 
+# TODO: This needs to be renamed to MTBDatasetLoad, then create
+# the actual PineConeUpload class for upserting the datasets
+
 # initialize pinecone, create the index
+"""
 pinecone.init(
     api_key=api_key,
     environment=env_key
