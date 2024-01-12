8ecdcdb9b6e7b8c16f2a193a658adef197f8ff2f
jandog8990 <jandog8990@gmail.com>
2024-01-11 20:07:24 -0700

    MOved directories to external repositories to show off in my portfolio

--- ParseMTBProject/HuggingFace/LoggingExample.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.debug("A debug message")
-logging.info("An info message")
-logging.warning("A warning message")
-logging.error("An error message")
-logging.critical("A critical message")
--- VectorDatabases/LoadSquadDataset.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from SquadDataset import SquadDataset
-
-# load dataset and pickle
-obj = SquadDataset()
-dataset = obj.loadSQUAD()
-with open('squad.pkl', 'wb') as f:
-    pickle.dump(dataset, f)
